4c697a24687d8d25a2ee836a561e7933dacaa5e6
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-02 16:00:15 -0800

    fixed error in bio

--- Main/src/components/About/index.js
+++ Main/src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-       Im a software engineer based out of the Bay Area. My skills include index.html, css, javascript, insomnia, mongo db, React and many more other technologies! When Im not working I enjoy producing music and also I create social media content with my brother as well. Some things I enjoy is traveling and working out. A motto i live by is that hard work does pay off. Never give up and continue to push!
+       Im a software engineer based out of the Bay Area. My skills include index.html, css, javascript, insomnia, mongo db, React and many other technologies! When Im not working I enjoy producing music and also I create social media content with my brother as well. Some things I enjoy is traveling and working out. A motto i live by is that hard work does pay off. Never give up and continue to push!
         </p>
       </div>
     </section>
