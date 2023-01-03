5e6f016613a81a00530b085f99cb108d7e4ccbfc
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-02 18:55:09 -0800

    wanted to go with a diffrent border for the bio pic

--- Main/src/components/About/index.js
+++ Main/src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-       <img src={coverImage} style={{width:"300px", height:"auto", borderRadius:"25%"}}></img>
+       <img src={coverImage} style={{width:"300px", height:"auto"}}></img>
         </div>
         <p>
        Im a software engineer based out of the Bay Area who gradutaed from California State University East Bay. My skills include index.html, css, javascript, insomnia, mongo db, React and many other technologies! When Im not working I enjoy producing music and also I create social media content with my brother as well. Some things I enjoy is traveling and working out. A motto i live by is that hard work does pay off. Never give up and continue to push!
